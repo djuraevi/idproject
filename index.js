b28a69a15be5907debd20742d75a3e9b0d3ee621
Djuraev Iskander <djuraevi@yandex.ru>
2021-10-29 22:31:14 +0300

    add js
    add js

--- index.js
+++ index.js
@@ -1,37 +1,45 @@
 
-const button = document.querySelector('.form__button');
-button.addEventListener("click", createCard);
+const buttonAdd = document.querySelector('.form__button');
+buttonAdd.addEventListener("click", createCard);
+
+const buttonRemove = document.querySelector('.item__button');
+buttonRemove.addEventListener("click", removeCard);
+
+function removeCard () {
+    const cardItem = document.querySelector('.item');
+    cardItem.remove();
+}
 
 
 function createCard() {
 
     const cardContainer = document.querySelector('.content');
     const cardItem = document.createElement('div');
+    const buttonElement = document.createElement('input');
     const imgElement = document.createElement('img');
     const nameElement = document.createElement('h1');
     const descriptionElement = document.createElement('p');
     const priceElement = document.createElement('p');
-    const buttonElement = document.createElement('button');
-    
     
     cardItem.classList.add('item');
+    buttonElement.classList.add('item__button');
     imgElement.classList.add('item__img');
     nameElement.classList.add('item__tittle');
     descriptionElement.classList.add('item__description');
     priceElement.classList.add('item__price');
-    buttonElement.classList.add('item__button');
-
+    
     // imgElement.textContent = imgValue;
     // nameElement.textContent = nameValue;
     // descriptionElement.textContent = descriptionValue;
     // priceElement.textContent = priceValue;
-    
-    
+     
     cardContainer.appendChild(cardItem);
+    cardItem.appendChild(buttonElement); 
+    cardItem.appendChild(imgElement);
     cardItem.appendChild(nameElement);
     cardItem.appendChild(descriptionElement);
     cardItem.appendChild(priceElement);
-    cardItem.appendChild(buttonElement); 
+    
 }
 
 
@@ -39,26 +47,3 @@ function createCard() {
 
 
 
-// window.onload = init;
-
-// function init() {
-// 	var button = document.querySelector(".form__button");
-//     button.onclick = buttonClickHandler;
-// }
-
-// function buttonClickHandler() {
-// 	var textInput = document.querySelector(".form__name");
-//     var catName = textInput.value;
-	
-// 	// если строка пустая
-// 	if(catName == "") {
-// 		alert("Введите имя кота, пожалуйста");
-// 	}else {
-//         //alert("Добавлен " + catName);
-// 		var div = document.createElement("div");
-//         tittle.innerHTML = catName;
-// 		var cardContent = document.querySelector(".content");
-//         cardContent.appendChild(div);
-// 		textInput.value = "";
-//     }
-// }
